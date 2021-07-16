8e49a37f89ab57dc630a595161a512010dd335d3
FelipeMG-16 <felipemaquedag@gmail.com>
2021-07-16 15:22:09 -0500

    Teoría de Operadores Lógicos

---
+++ OperadoresLogicos/src/OperadoresLogicos.java
@@ -0,0 +1,30 @@
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Operadores lógicos
+		 * a == b a es igual a b
+		 * a > b a es mayor que b
+		 * a < b es menor que
+		 * a ! = b a es diferente a b
+		 *
+		 *! (not)
+		 *| (or)
+		 *&
+		 *|| (short circuit) evalua a y luego evalua b
+		 *&&
+		 *
+		 */
+		
+		System.out.println(saludo());
+	
+	}
+	
+	//
+	public static String saludo () {
+		return "saludo";
+	}
+
+}
